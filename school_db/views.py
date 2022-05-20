49f86c7e49b9ed762a0f752956e0220887952e09
rupert-ma <michael.a.rupert@gmail.com>
2022-05-20 13:05:18 -0700

    problem six added

--- school_db/views.py
+++ school_db/views.py
@@ -288,7 +288,12 @@ def problem_six(request):
     
     # Make sure to set this equal to the primary key of the row you just created!
     student_id = 11
+    Student.objects.filter(id=student_id).update(gpa=3.9)
+    student = Student.objects.get(id=11)
 
+    print(f'ID: {student.id}')
+    print(f'Full Name: {student.first_name} {student.last_name}')
+    print(f'GPA: {student.gpa}')
 
 
     return complete(request)
